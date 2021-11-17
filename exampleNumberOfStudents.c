9110280002db10f4efc5cb60760356c2e9e8fcf6
AHMAD-KHAIRI <ahmadkhairi_hamzah85@yahoo.com>
2021-11-18 00:52:44 +0800

    declared variabls for boys, girls and students and printed in console

--- exampleNumberOfStudents.c
+++ exampleNumberOfStudents.c
@@ -3,5 +3,13 @@
 
 int main()
 {
+    int girls = 12;
+    int boys = 9;
+    int students = girls + boys;
+
+    printf("There are %d boys\n", boys);
+    printf("There are %d girls\n", girls);
+    printf("Total number of students is %d\n", students);
+
     return EXIT_SUCCESS;
 }
