28357917f291332dec7993d0becd21494e2e4b33
AHMAD-KHAIRI <ahmadkhairi_hamzah85@yahoo.com>
2021-11-18 01:37:52 +0800

    print values in console

--- example1UnsignedInt.c
+++ example1UnsignedInt.c
@@ -6,5 +6,8 @@ int main()
     unsigned students = 25U;
     unsigned long long worldPopulation = 7801235945UL;
 
+    printf("Total number of students : %u\n", students);
+    printf("World population : %llu\n", worldPopulation);
+
     return EXIT_SUCCESS;
 }
